575f051470bc1fa8f219a3edb8bd79343a17396f
Dale Francum <dale.francum@ncino.com>
2018-02-11 18:33:52 -0500

    Added cfsorba class and tests.

---
+++ cfsorba_alexis/cfsorba.py
@@ -0,0 +1,52 @@
+import logging
+import os
+import re
+from bs4 import BeautifulSoup
+import requests
+
+
+logging.basicConfig(level=os.environ.get("LOGLEVEL", logging.WARN))
+
+class CapeFearSorba(object):
+
+    document_url = os.environ.get("CFSORBA_DOCUMENT_URL", "http://capefearsorba.org")
+    list_item_regex = "<li class=\"clearfix\">.*<p>([A-z .]+) .*(OPEN|CLOSED).*</li>"
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def get_document_html(document_url):
+        """
+        Given a URL, retrieve the document and return its HTML
+        :return:
+        """
+        document_html = requests.get(url=document_url)
+        return document_html
+
+    def _parse_html(self, html_doc):
+
+        open_trails = []
+        closed_trails = []
+
+        soup = BeautifulSoup(html_doc, "html.parser")
+        ul_element = soup.find(id="edn-effect-slider")
+
+        for list_item in ul_element.find_all("li"):
+
+            logging.debug(str(list_item))
+
+            regex = re.search(self.list_item_regex, str(list_item), re.I | re.S)
+            if regex:
+                trail = regex.group(1)
+                status = regex.group(2)
+                if status == "OPEN":
+                    open_trails.append(trail)
+                elif status == "CLOSED":
+                    closed_trails.append(trail)
+                else:
+                    logging.warn("Could not determine status for trail:\nTrail: %s\nStatus: %s" % (trail, status))
+            else:
+                logging.warn("Could not parse list item html:\n%s" % str(list_item))
+
+        return {"open": open_trails, "closed": closed_trails}
---
+++ tests/test_cfsorba.py
@@ -0,0 +1,34 @@
+import unittest
+from mock import patch
+import cfsorba_alexis.cfsorba as cfsorba
+
+
+class TestCapeFearSorba(unittest.TestCase):
+
+    @patch('cfsorba_alexis.cfsorba.requests')
+    def test_get_document_html(self, mock_requests):
+        test_html = "<html></html>"
+        mock_requests.get.return_value = test_html
+        document_html = cfsorba.CapeFearSorba.get_document_html(cfsorba.CapeFearSorba.document_url)
+        self.assertEqual(test_html, document_html)
+
+    def test_parse_html(self):
+
+        test_html_file = "tests/data/cfsorba_home.20170211.html"
+        with file(test_html_file) as f:
+            test_html = f.read()
+
+        cfs = cfsorba.CapeFearSorba()
+        response = cfs._parse_html(html_doc=test_html)
+
+        expected_response = {
+            "open": [
+                "Brunswick Nature Park",
+                "Browns Creek",
+                "Horry Co. Bike Park"
+            ],
+            "closed": [
+                "Blue Clay Bike Park"
+            ]
+        }
+        self.assertDictEqual(expected_response, response)
